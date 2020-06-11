41516fe90289bcd2b032ca98d1ae077825105da2
LalitaKashyap <54372570+LalitaKashyap@users.noreply.github.com>
2020-06-12 00:41:46 +0530

    Updated file name to TEST.py
    Also some gibberish  and hello from my side 💯 :-D

---
+++ TEST.py
@@ -0,0 +1,7 @@
+print("HEllo world:")
+#Hello ji
+print("NICE TO MEET YOU..........")
+print("-----------------*******************-------------------------**********")
+print("PLease dont spoil it for me")
+print("THis is the fourth line")
+print("************-------------------****************************--------------")
--- lali.py
+++
@@ -1,4 +0,0 @@
-print("HEllo world:")
-print("NICE TO MEET YOU..........")
-print("PLease dont spoil it for me")
-print("THis is the fourth line")
